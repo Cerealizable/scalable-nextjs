364c8a17c7142c4673bfcfdf81ea246575567f53
cereal <alberstc1@gmail.com>
2022-04-11 14:01:21 -0700

    feat: created DogCard

--- components/cards/dog/DogCard.mocks.ts
+++ components/cards/dog/DogCard.mocks.ts
@@ -1,7 +1,11 @@
 import { IDogCard } from './DogCard';
 
 const base: IDogCard = {
-  sampleTextProp: 'Hello world!',
+  tag: 'Doggos',
+  title: `Why vizla's are so great!`,
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  author: 'Alex',
+  time: '2h ago',
 };
 
 export const mockDogCardProps = {
